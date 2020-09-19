032decdd34bb5bef826731f5df52f2cba2d191b2
Raunaq <raunaq.soni@gmail.com>
2020-09-19 19:54:18 +0530

    Vid 1 - basic grid done

---
+++ bfs-viz/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import BFS from './BFS/BFS';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <BFS />
+    </div>
+  );
+}
+
+export default App;
